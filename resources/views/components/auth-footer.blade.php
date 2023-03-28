f17e7ab0078d8765e044bb9572addd20e91f493d
yeremia wahyu <yeremiawahyu009@gmail.com>
2023-03-29 03:07:38 +0700

    add file

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; 2022 <div class="bullet"></div> PT. Bringin Gigantara Cabang Cempaka Putih
+  </div>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2022 <div class="bullet"></div> PT. Bringin Gigantara Cabang Cempaka Putih
+    </div>
+    <div class="footer-right">
+        1.0.0
+    </div>
+</footer>
